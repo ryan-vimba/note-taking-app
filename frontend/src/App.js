ead64ee5ec792ffbc609f7b63d34df3cebc86957
emiliafrench <emilia.french@petalcard.com>
2023-09-24 18:00:33 -0400

    use useGoogleLogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,19 @@
 import React from 'react';
 import GoogleOAuth from './GoogleLogin';
 import api from './ApiService';
-
+import axios from 'axios';
+import { useGoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 function App() {
-  const handleGoogleSuccess = (userProfile) => {
-    console.log('Logged in with Google:', userProfile);
-    // Make an API request to your FastAPI backend to save or verify the user's credentials.
-    api.post('/user/login', userProfile).then((response) => {
-      // Handle the response from the backend.
-    });
-  };
 
-  const handleGoogleFailure = () => {
-    console.log('Google login failed');
-  };
+  const login = useGoogleLogin({
+    onSuccess: codeResponse => console.log(codeResponse),
+    flow: 'auth-code',
+  });
 
   return (
-    <div>
-      <h1>React Frontend with Google OAuth and FastAPI Backend</h1>
-      <GoogleOAuth onSuccess={handleGoogleSuccess} onFailure={handleGoogleFailure} />
-    </div>
-  );
+      <button onClick={login}>Sign in with Google</button>
+  )
+
 }
 
 export default App;
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- frontend/src/GoogleLogin.jsx
+++ frontend/src/GoogleLogin.jsx
@@ -1,28 +1,48 @@
-import React from 'react';
-import { GoogleLogin } from 'react-google-login';
+// import React from 'react';
+// import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+// import { useGoogleLogin } from '@react-oauth/google';
 
-const GoogleOAuth = ({ onSuccess, onFailure }) => {
-  const responseGoogle = (response) => {
-    if (response && response.profileObj) {
-      onSuccess(response.profileObj);
-    } else {
-      console.log(response)
-      onFailure();
-    }
-  };
 
-  return (
-    <div>
-      <GoogleLogin
-        clientId="669500394322-bmrc3fop25pm2mtc6er23f5h7hs946kg.apps.googleusercontent.com"
-        buttonText="Login with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
-    </div>
-  );
-};
+// const GoogleOAuth = ({ onSuccess, onFailure }) => {
+//   // const login = useGoogleLogin({
+//   //   clientId: "669500394322-bmrc3fop25pm2mtc6er23f5h7hs946kg.apps.googleusercontent.com",
+//   //   onSuccess: codeResponse => console.log(codeResponse),
+//   //   flow: 'auth-code',
+//   // });
+  
+//   // const GoogleLoginButton = () => {
+//   //   const { signIn } = login()
+//   //   return <button onClick={signIn}>Sign in with Google</button>
+//   // }
+  
+//     return (
+//       <GoogleOAuthProvider clientId="669500394322-bmrc3fop25pm2mtc6er23f5h7hs946kg.apps.googleusercontent.com">
+//         <button onClick={login()}>Sign in with Google
+//         </button>
+//       </GoogleOAuthProvider>)
+//   // const responseGoogle = (response) => {
+//   //   console.log(response)
+//   //   if (response & response.tokenId) {
+//   //     onSuccess(response.tokenId);
+//   //   } else {
+//   //     onFailure();
+//   //   }
+//   // };
 
-export default GoogleOAuth;
+//   // return (
+//   //   <div>
+//   //     <GoogleOAuthProvider clientId="669500394322-bmrc3fop25pm2mtc6er23f5h7hs946kg.apps.googleusercontent.com">
+//   //     <GoogleLogin
+//   //       clientId="669500394322-bmrc3fop25pm2mtc6er23f5h7hs946kg.apps.googleusercontent.com"
+//   //       buttonText="Login with Google"
+//   //       onSuccess={responseGoogle}
+//   //       onFailure={responseGoogle}
+//   //       cookiePolicy={'single_host_origin'}
+//   //     />
+//   //     </GoogleOAuthProvider>
+//   //   </div>
+//   // );
+// };
+
+// export default GoogleOAuth;
 
