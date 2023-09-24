a2d5961583aa0e723941749a79c67b3fff0b917e
Ryan Vimba <Ryan.Vimba@ibm.com>
2023-09-24 18:04:31 -0400

    update oauth

--- backend/main.py
+++ backend/main.py
@@ -1,63 +1,76 @@
-from fastapi import FastAPI, Depends, HTTPException
-from authlib.integrations.starlette_client import OAuth
+from fastapi import FastAPI
 from starlette.config import Config
 from starlette.requests import Request
 from database import SessionLocal, get_or_create_user
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
+import httpx
 
 app = FastAPI()
 
-# Load your Google OAuth2 credentials (You can use environment variables or a config file)
+
 config = Config('.env')
 GOOGLE_CLIENT_ID = config('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = config('GOOGLE_CLIENT_SECRET')
 GOOGLE_REDIRECT_URI = config('GOOGLE_REDIRECT_URI')
+SECURE_KEY = config('SECURE_KEY')
 
-oauth = OAuth(config)
-oauth.register(
-    name='google',
-    client_id=GOOGLE_CLIENT_ID,
-    client_secret=GOOGLE_CLIENT_SECRET,
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    client_kwargs={'scope': 'email profile'}
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(SessionMiddleware, secret_key=SECURE_KEY)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.get('/login/')
-async def login(request: Request):
-    # Generate the Google OAuth2 authorization URL and redirect user
-    redirect_uri = GOOGLE_REDIRECT_URI
-    return await oauth.google.authorize_redirect(request, redirect_uri)
+@app.get("/auth")
+async def auth(request: Request, code: str):
+    # URL to exchange code for access token
+    token_endpoint = 'https://oauth2.googleapis.com/token'
+
+    # Payload contains the details for the POST request
+    payload = {
+        'client_id': GOOGLE_CLIENT_ID,
+        'client_secret': GOOGLE_CLIENT_SECRET,
+        'code': code,
+        'redirect_uri': GOOGLE_REDIRECT_URI,
+        'grant_type': 'authorization_code'
+    }
+
+    # Make the POST request to get the tokens
+    response = httpx.post(token_endpoint, data=payload)
+    token_data = response.json()
+
+    # Extract the access token and ID token
+    access_token = token_data.get('access_token')
+    id_token_str = token_data.get('id_token')
+
+    # Verify the ID token
+    id_info = id_token.verify_oauth2_token(id_token_str, requests.Request(), GOOGLE_CLIENT_ID)
+    
+    if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+        raise ValueError('Wrong issuer.')
+    
+    # Save the email to the session or do something else
+    request.session['user'] = {
+        "email": id_info["email"]
+    }
+
+    return {"message": "Successfully authenticated"}
 
 
-@app.get('/auth/')
-async def authorize(request: Request):
-    try:
-        # Fetch the access token
-        token = await oauth.google.authorize_access_token(request)
-
-        # Fetch the user's info using the access token
-        user_info = await oauth.google.parse_id_token(request, token)
-        
-        # Initiate a new DB session
-        session = SessionLocal()
-        
-        # At this point, you can check if this user exists in your database
-        email = user_info.get('email')
-        name = user_info.get('name')
-        
-        # If they don't exist, create a new user record
-        user = get_or_create_user(session, email, name)
-        
-        # Close DB session
-        session.close()
-        
-        # You might also want to create a session or JWT token to keep the user logged in
-
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-
-    return {"message": "Successfully authenticated", "user": user_info}
+@app.get('/')
+def check(request:Request):
+    user = request.session.get('user')
+    if not user:
+        return {"error": "Not authenticated"}
+    return "hi "+ str(request.session.get('user')['email'])
